faa6fffab5b0b9e0944820fe3575b4b9c8fbc4f3
Daniel Nevin Richards <dnevinrichards@berkeley.edu>
2024-11-19 01:34:03 -0800

    set debug to true so I can interact with the API

--- accumate_backend/settings.py
+++ accumate_backend/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY", default="change_me")  
 
-DEBUG = env("DEBUG", default=False)  
+DEBUG = env("DEBUG", default=True)  
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
