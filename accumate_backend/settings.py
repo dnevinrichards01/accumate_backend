c514f76c4acdb0d07d7beacb341fc94d7fda5473
Daniel Nevin Richards <dnevinrichards@berkeley.edu>
2024-12-08 19:19:00 -0800

    ssl: True in CACHES in settings.py

--- accumate_backend/settings.py
+++ accumate_backend/settings.py
@@ -129,7 +129,8 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "CONNECTION_POOL_KWARGS": {
-                "ssl_cert_reqs": "CERT_NONE",
+                "ssl": True,
+                "ssl_cert_reqs": None,
             },
         },
     }
